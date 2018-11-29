45ca3955f1c402db43df3c4dad02111cc7b9dde1
Brian Denicola <bjd145@yahoo.com>
2018-11-29 07:57:20 -0600

    Added Azure Files example

---
+++ AKS/update_storageaccount_secret.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+export k8s=$1
+export rg=$2
+export storageacct=$
+
+az login
+
+nrg=$(az aks show --resource-group $rg --name $k8s --query nodeResourceGroup -o tsv)
+key=$(az storage account keys list  -n $storageacct -g $nrg -o json --query "[0].value" | sed -e 's/^"//' -e 's/"$//')
+secret_name=$(kubectl get secret -o json | jq ".items[] | select(.metadata.name | contains(\"$storageacct\")) | .metadata.name" | sed -e 's/^"//' -e 's/"$//')
+
+kubectl delete secret $secret_name
+kubectl create secret generic $secret_name --from-literal=azurestorageaccountname=$storageacct --from-literal=azurestorageaccountkey=$key
