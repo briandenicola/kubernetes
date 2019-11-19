b58a1ab0a278e47327cf18ebe542151958d73a59
Brian Denicola <bjd145@yahoo.com>
2019-11-19 08:38:12 -0600

    replaced sed with tr

--- AKS/update_storageaccount_secret.sh
+++ AKS/update_storageaccount_secret.sh
@@ -7,8 +7,8 @@ export storageacct=$3
 az login
 
 nrg=$(az aks show --resource-group $rg --name $k8s --query nodeResourceGroup -o tsv)
-key=$(az storage account keys list  -n $storageacct -g $nrg -o json --query "[0].value" | sed -e 's/^"//' -e 's/"$//')
-secret_name=$(kubectl get secret -o json | jq ".items[] | select(.metadata.name | contains(\"$storageacct\")) | .metadata.name" | sed -e 's/^"//' -e 's/"$//')
+key=$(az storage account keys list  -n $storageacct -g $nrg -o json --query "[0].value" | tr -d \" )
+secret_name=$(kubectl get secret -o json | jq ".items[] | select(.metadata.name | contains(\"$storageacct\")) | .metadata.name" | tr -d \")
 
 kubectl delete secret $secret_name
 kubectl create secret generic $secret_name --from-literal=azurestorageaccountname=$storageacct --from-literal=azurestorageaccountkey=$key
