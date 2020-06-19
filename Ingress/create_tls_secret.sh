c6dd34db546a121a7dad8417f71323dc34fb1dcd
Brian Denicola <bjd145@yahoo.com>
2020-06-18 22:46:35 -0500

    Moved to azure repo

--- Ingress/create_tls_secret.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash 
-
-#helm upgrade traefik stable/traefik --set ssl.insecureSkipVerify=true --set ssl.enabled=true --set rbac.enabled=true
-#acme.sh --issue -d *.bjdazure.tech --yes-I-know-dns-manual-mode-enough-go-ahead-please --dns 
-
-kubectl create secret tls traefik-apim-cert --key=wildcard.bjdazure.tech.key --cert=wildcard.bjdazure.tech.cer
