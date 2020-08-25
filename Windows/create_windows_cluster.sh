52239e3502329c88a71c64c48cd9caf1a779277f
Brian Denicola <bjd145@yahoo.com>
2020-08-25 08:23:57 -0500

    Updated windows creation script for MSI

--- Windows/create_windows_cluster.sh
+++ Windows/create_windows_cluster.sh
@@ -2,21 +2,23 @@
 
 export NAME=$1
 export WIN_PASSWORD=$2
-export CLIENT_SECRET=$3
 
-RG_NAME='DevSub02_K8S_RG'
+RG_NAME='DevSub02_K8S01_RG'
+NODEPOOL_RG_NAME='DevSub02_K8S01_RG_winnodes'
 LINUX_NODE_POOL='lpool1'
 WIN_NODE_POOL='wpool1'
-CLIENT_ID='4e565daf-621d-48d3-b010-1208da519cbe'
 SERVICE_CIDR='10.191.0.0/16'
 DNS_IP='10.191.0.10'
 SUBNET_ID='/subscriptions/bfafbd89-a2a3-43a5-af72-fb4ef0c514c1/resourceGroups/DevSub02_Network_RG/providers/Microsoft.Network/virtualNetworks/DevSub02-VNet-002/subnets/KubernetesWindows'
 VER='1.17.7'
 
+PUBLIC_IP=`curl -s http://checkip.amazonaws.com/`
+
 az aks create \
     --resource-group $RG_NAME \
     --name $NAME \
     --node-count 1 \
+    --node-resource-group $NODEPOOL_RG_NAME \
     --enable-addons monitoring \
     --windows-admin-password $WIN_PASSWORD \
     --windows-admin-username manager \
@@ -24,8 +26,6 @@ az aks create \
     --nodepool-name $LINUX_NODE_POOL \
     --network-plugin azure \
     --vnet-subnet-id $SUBNET_ID \
-    --service-principal $CLIENT_ID \
-    --client-secret $CLIENT_SECRET \
     --ssh-key-value '~/.ssh/id_rsa.pub' \
     --kubernetes-version $VER \
     --service-cidr $SERVICE_CIDR \
@@ -34,11 +34,14 @@ az aks create \
     --min-count 1 \
     --max-count 3 \
     --load-balancer-sku Standard \
+    --uptime-sla \
+	--enable-managed-identity \
+    --api-server-authorized-ip-ranges $PUBLIC_IP/32 
 
 az aks nodepool add \
     --resource-group $RG_NAME \
     --cluster-name $NAME \
     --os-type Windows \
     --name $WIN_NODE_POOL \
-    --node-count 1 \
+    --node-count 3 \
     --kubernetes-version $VER
