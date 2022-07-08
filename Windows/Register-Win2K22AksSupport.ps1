688d0d7532e26b9dca6c856ac23738ecb52553b4
briandenicola <brian@briandenicola.com>
2022-07-08 10:28:14 -0500

    Script to Register Win2k22 support on AKS

---
+++ Windows/Register-Win2K22AksSupport.ps1
@@ -0,0 +1,22 @@
+param(
+    [Parmeter(Mandatory=$true)]
+    [string] $FeatureName = "AKSWindows2022Preview", 
+    [Parmeter(Mandatory=$true)]
+    [string] $NameSpace = "Microsoft.ContainerService"
+)
+az feature register --namespace $NameSpace --name $FeatureName
+
+function Get-FeatureState 
+{
+    param(
+        [string] $FeatureName
+    )
+    return (az feature list -o tsv --query "[?contains(name,'$FeatureName')].`{State:properties.state}")
+}
+
+while ( Get-FeatureState -FeatureName "$NameSpace/$FeatureName" -eq "Registering" ) {
+    Write-Host "." -NoNewline
+    Start-Sleep -Seconds 60
+}
+
+az provider register --namespace Microsoft.ContainerService
