5606f8cbe9cd871ab9223fe93e1ebf3b5863e3ec
briandenicola <brian@briandenicola.com>
2023-01-06 18:16:16 -0600

    New taskfile to manage environment

--- AKS/azureml-extension/outputs.tf
+++ AKS/azureml-extension/outputs.tf
@@ -7,3 +7,13 @@ output "AKS_CLUSTER_NAME" {
     value = azurerm_kubernetes_cluster.this.name
     sensitive = false
 }
+
+output "AKS_CLUSTER_ID" {
+    value = azurerm_kubernetes_cluster.this.id
+    sensitive = false
+}
+
+output "ML_WORKSPACE_NAME" {
+    value = azurerm_machine_learning_workspace.this.name
+    sensitive = false
+}
