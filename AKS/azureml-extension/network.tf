45a1cfe049a4deed339377bad8d8c7c5853314d9
briandenicola <brian@briandenicola.com>
2023-01-09 11:43:05 -0600

    Disable network policies

--- AKS/azureml-extension/network.tf
+++ AKS/azureml-extension/network.tf
@@ -1,6 +1,6 @@
 resource "azurerm_virtual_network" "this" {
   name                = "${local.resource_name}-network"
-  address_space       = [ local.vnet_cidr ]
+  address_space       = [local.vnet_cidr]
   location            = azurerm_resource_group.this.location
   resource_group_name = azurerm_resource_group.this.name
 }
@@ -9,23 +9,23 @@ resource "azurerm_subnet" "nodes" {
   name                 = "nodes"
   resource_group_name  = azurerm_resource_group.this.name
   virtual_network_name = azurerm_virtual_network.this.name
-  address_prefixes     = [ local.nodes_subnet_cidir ]
+  address_prefixes     = [local.nodes_subnet_cidir]
 }
 
 resource "azurerm_subnet" "pods" {
   name                 = "pods"
   resource_group_name  = azurerm_resource_group.this.name
   virtual_network_name = azurerm_virtual_network.this.name
-  address_prefixes     = [ local.pods_subnet_cidir ]
-  
+  address_prefixes     = [local.pods_subnet_cidir]
+
   delegation {
     name = "aks-delegation"
 
     service_delegation {
-      name    = "Microsoft.ContainerService/managedClusters"
+      name = "Microsoft.ContainerService/managedClusters"
       actions = [
         "Microsoft.Network/virtualNetworks/subnets/join/action",
-      ]  
+      ]
     }
   }
 }
@@ -34,14 +34,15 @@ resource "azurerm_subnet" "pe" {
   name                 = "private-endpoints"
   resource_group_name  = azurerm_resource_group.this.name
   virtual_network_name = azurerm_virtual_network.this.name
-  address_prefixes     = [ local.pe_subnet_cidir ]
+  address_prefixes     = [local.pe_subnet_cidir]
 }
 
 resource "azurerm_subnet" "compute" {
-  name                 = "compute"
-  resource_group_name  = azurerm_resource_group.this.name
-  virtual_network_name = azurerm_virtual_network.this.name
-  address_prefixes     = [ local.compute_subnet_cidir ]
+  name                                      = "compute"
+  resource_group_name                       = azurerm_resource_group.this.name
+  virtual_network_name                      = azurerm_virtual_network.this.name
+  address_prefixes                          = [local.compute_subnet_cidir]
+  private_endpoint_network_policies_enabled = true
 }
 
 
