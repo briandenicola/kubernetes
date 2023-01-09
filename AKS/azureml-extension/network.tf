a89a59cbf9ef604e670de580cc81642a85c2a3bd
briandenicola <brian@briandenicola.com>
2023-01-09 11:48:37 -0600

    Disabled Private Link Service endpoint policies

--- AKS/azureml-extension/network.tf
+++ AKS/azureml-extension/network.tf
@@ -38,11 +38,12 @@ resource "azurerm_subnet" "pe" {
 }
 
 resource "azurerm_subnet" "compute" {
-  name                                      = "compute"
-  resource_group_name                       = azurerm_resource_group.this.name
-  virtual_network_name                      = azurerm_virtual_network.this.name
-  address_prefixes                          = [local.compute_subnet_cidir]
-  private_endpoint_network_policies_enabled = false
+  name                                          = "compute"
+  resource_group_name                           = azurerm_resource_group.this.name
+  virtual_network_name                          = azurerm_virtual_network.this.name
+  address_prefixes                              = [local.compute_subnet_cidir]
+  private_endpoint_network_policies_enabled     = false
+  private_link_service_network_policies_enabled = false
 }
 
 
