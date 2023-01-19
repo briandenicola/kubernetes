715297796cad2f3188420041192a60c1bde9b606
briandenicola <brian@briandenicola.com>
2023-01-19 11:06:01 -0600

    Removed old examples

--- AKS/keyvault-csi-example/identityv2-creation.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-export IDENTITY=$1
-export RGNAME=$2
-export AKSNAME=$3
-
-RESOURCEID=`az identity show --name $IDENTITY --resource-group $RGNAME --query id -o tsv`
-az aks pod-identity add --resource-group $RGNAME --cluster-name $AKSNAME --namespace default --name $IDENTITY --identity-resource-id $RESOURCEID
