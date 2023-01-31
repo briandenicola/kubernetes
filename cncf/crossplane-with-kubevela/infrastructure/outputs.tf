c995c850c03fbfba52c3b1b45f9e804e89a17cfb
briandenicola <brian@briandenicola.com>
2023-01-31 10:43:02 -0600

    Added subscription output

--- cncf/crossplane-with-kubevela/infrastructure/outputs.tf
+++ cncf/crossplane-with-kubevela/infrastructure/outputs.tf
@@ -12,3 +12,8 @@ output "WORKLOAD_AKS_CLUSTER_NAME" {
   value     = azurerm_kubernetes_cluster.workload.name
   sensitive = false
 }
+
+output "AKS_SUBSCRIPTION_ID" {
+  value     = data.azurerm_client_config.current.subscription_id
+  sensitive = false
+}
