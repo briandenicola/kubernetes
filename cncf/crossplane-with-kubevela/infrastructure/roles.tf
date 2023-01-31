75d0845bd58c677031238fcf8e815f77a3b8cfae
briandenicola <brian@briandenicola.com>
2023-01-31 10:45:14 -0600

    Misspelling

--- cncf/crossplane-with-kubevela/infrastructure/roles.tf
+++ cncf/crossplane-with-kubevela/infrastructure/roles.tf
@@ -4,28 +4,28 @@ resource "azurerm_role_assignment" "cluster_owner" {
   principal_id         = data.azurerm_client_config.current.object_id
 }
 
-resource "azurerm_role_assignment" "controlplane_role_assignemnt_nework" {
+resource "azurerm_role_assignment" "controlplane_role_assignment_network" {
   scope                            = azurerm_virtual_network.this.id
   role_definition_name             = "Network Contributor"
   principal_id                     = azurerm_user_assigned_identity.controlplane_identity.principal_id
   skip_service_principal_aad_check = true
 }
 
-resource "azurerm_role_assignment" "controlplane_role_assignemnt_msi" {
+resource "azurerm_role_assignment" "controlplane_role_assignment_msi" {
   scope                            = azurerm_user_assigned_identity.controlplane_kubelet_identity.id
   role_definition_name             = "Managed Identity Operator"
   principal_id                     = azurerm_user_assigned_identity.controlplane_identity.principal_id
   skip_service_principal_aad_check = true
 }
 
-resource "azurerm_role_assignment" "aks_role_assignemnt_nework" {
+resource "azurerm_role_assignment" "aks_role_assignment_network" {
   scope                            = azurerm_virtual_network.this.id
   role_definition_name             = "Network Contributor"
   principal_id                     = azurerm_user_assigned_identity.aks_identity.principal_id
   skip_service_principal_aad_check = true
 }
 
-resource "azurerm_role_assignment" "aks_role_assignemnt_msi" {
+resource "azurerm_role_assignment" "aks_role_assignment_msi" {
   scope                            = azurerm_user_assigned_identity.aks_kubelet_identity.id
   role_definition_name             = "Managed Identity Operator"
   principal_id                     = azurerm_user_assigned_identity.aks_identity.principal_id
