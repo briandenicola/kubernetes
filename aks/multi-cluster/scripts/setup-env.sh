c868b906963e8b83a4b25d610da2cb36575082f2
briandenicola <brian@briandenicola.com>
2023-02-08 14:54:29 -0600

    Adjusted to fig new submodule

--- aks/multi-cluster/scripts/setup-env.sh
+++ aks/multi-cluster/scripts/setup-env.sh
@@ -1,3 +1,7 @@
-export RG=$(terraform -chdir=./infrastructure output -raw AKS_RESOURCE_GROUP)
-export AKS=$(terraform -chdir=./infrastructure output -raw AKS_CLUSTER_NAME)
-export AKS_CLUSTER_ID=$(terraform -chdir=./infrastructure output -raw AKS_CLUSTER_ID)
+SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
+INFRA_PATH=$(realpath "${SCRIPT_DIR}/../infrastructure/infrastructure")
+
+export RG=$(terraform -chdir=${INFRA_PATH} output -raw AKS_RESOURCE_GROUP)
+export AKS=$(terraform -chdir=${INFRA_PATH} output -raw AKS_CLUSTER_NAME)
+
+export AKS_CLUSTER_ID=$(az aks show -n ${AKS} -g ${RG} -o tsv --query id)
