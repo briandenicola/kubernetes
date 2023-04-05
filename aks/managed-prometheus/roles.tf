2cf573e761ad49cbee7d7a107f7061956867f6a6
briandenicola <brian@briandenicola.com>
2023-04-05 12:01:24 -0500

    Added Grafana Admin

--- aks/managed-prometheus/roles.tf
+++ aks/managed-prometheus/roles.tf
@@ -25,3 +25,9 @@ resource "azurerm_role_assignment" "grafana_monitoring_data_read" {
   principal_id         = azurerm_dashboard_grafana.this.identity[0].principal_id 
   skip_service_principal_aad_check = true 
 }
+
+resource "azurerm_role_assignment" "grafana_admin" {
+  scope                = azurerm_resource_group.this.id
+  role_definition_name = "Grafana Admin"
+  principal_id         = data.azurerm_client_config.current.object_id
+}
