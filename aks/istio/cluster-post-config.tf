72d48039377a13d24abce92b705992c978bc5aff
briandenicola <brian@briandenicola.com>
2023-04-19 13:22:59 -0500

    Managed Istio Preview example

---
+++ aks/istio/cluster-post-config.tf
@@ -0,0 +1,28 @@
+resource "azapi_update_resource" "this" {
+  depends_on = [
+    azurerm_kubernetes_cluster.this
+  ]
+
+  type        = "Microsoft.ContainerService/managedClusters@2023-03-02-preview"
+  resource_id = azurerm_kubernetes_cluster.this.id
+
+  body = jsonencode({
+    properties = {
+      serviceMeshProfile = {
+        mode = "Istio"
+        istio = {
+          components = {
+            ingressGateways = [{
+              mode = "Internal",
+              enabled= true
+            }]
+            egressGateways = [{
+              mode = "Internal",
+              enabled= true
+            }]
+          }
+        }
+      }
+    }
+  })
+}
---
+++ aks/istio/cluster.tf
@@ -0,0 +1,95 @@
+resource "azurerm_kubernetes_cluster" "this" {
+  lifecycle {
+    ignore_changes = [
+      default_node_pool.0.node_count,
+    ]
+  }
+
+  name                            = local.aks_name
+  resource_group_name             = azurerm_resource_group.this.name
+  location                        = azurerm_resource_group.this.location
+  node_resource_group             = "${local.resource_name}_k8s_nodes_rg"
+  dns_prefix                      = local.aks_name
+  sku_tier                        = "Standard"
+  automatic_channel_upgrade       = "patch"
+  oidc_issuer_enabled             = true
+  workload_identity_enabled       = true
+  azure_policy_enabled            = true
+  local_account_disabled          = false 
+  open_service_mesh_enabled       = false
+  run_command_enabled             = false
+
+  api_server_access_profile {
+    vnet_integration_enabled = true
+    subnet_id                = azurerm_subnet.api.id
+  }
+
+  azure_active_directory_role_based_access_control {
+    managed                = true
+    azure_rbac_enabled     = true
+    tenant_id              = data.azurerm_client_config.current.tenant_id
+    admin_group_object_ids = [var.azure_rbac_group_object_id]
+  }
+
+  identity {
+    type         = "UserAssigned"
+    identity_ids = [azurerm_user_assigned_identity.aks_identity.id]
+  }
+
+  kubelet_identity {
+    client_id                 = azurerm_user_assigned_identity.aks_kubelet_identity.client_id
+    object_id                 = azurerm_user_assigned_identity.aks_kubelet_identity.principal_id
+    user_assigned_identity_id = azurerm_user_assigned_identity.aks_kubelet_identity.id
+  }
+
+  default_node_pool {
+    name                = "default"
+    node_count          = 1
+    vm_size             = "Standard_B4ms"
+    os_disk_size_gb     = 30
+    vnet_subnet_id      = azurerm_subnet.nodes.id
+    os_sku              = "CBLMariner"
+    type                = "VirtualMachineScaleSets"
+    enable_auto_scaling = true
+    min_count           = 1
+    max_count           = 3
+    max_pods            = 60
+    upgrade_settings {
+      max_surge         = "25%"
+    }  
+  }
+
+  network_profile {
+    dns_service_ip     = "100.${random_integer.services_cidr.id}.0.10"
+    service_cidr       = "100.${random_integer.services_cidr.id}.0.0/16"
+    network_plugin      = "azure"
+    network_plugin_mode = "Overlay"
+    load_balancer_sku   = "standard"
+    pod_cidr            = "192.168.0.0/16"
+  }
+
+  maintenance_window {
+    allowed {
+      day               = "Friday"
+      hours             = [21, 22, 22] 
+    }
+    allowed {
+      day               = "Sunday"
+      hours             = [1, 2, 3, 4, 5] 
+    }
+  }
+
+  auto_scaler_profile {
+    max_unready_nodes   = "1"
+  }
+  
+  oms_agent {
+    log_analytics_workspace_id = azurerm_log_analytics_workspace.this.id
+  }
+
+  microsoft_defender {
+    log_analytics_workspace_id = azurerm_log_analytics_workspace.this.id
+  }
+
+}
+
