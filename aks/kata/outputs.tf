2f750fc53a8c963df7c02a059d12a1cc4c58c895
briandenicola <brian@briandenicola.com>
2023-05-17 11:43:05 -0500

    Kata Container Examples

---
+++ aks/kata/outputs.tf
@@ -0,0 +1,14 @@
+output "AKS_RESOURCE_GROUP" {
+    value = azurerm_kubernetes_cluster.this.resource_group_name
+    sensitive = false
+}
+
+output "AKS_CLUSTER_NAME" {
+    value = azurerm_kubernetes_cluster.this.name
+    sensitive = false
+}
+
+output "AKS_CLUSTER_ID" {
+    value = azurerm_kubernetes_cluster.this.id
+    sensitive = false
+}
