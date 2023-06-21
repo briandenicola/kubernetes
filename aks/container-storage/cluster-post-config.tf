b0a173bd3fc134a1f019447dc0b0325b24635eb1
briandenicola <brian@briandenicola.com>
2023-06-21 09:57:34 -0500

    Formatting

--- aks/container-storage/cluster-post-config.tf
+++ aks/container-storage/cluster-post-config.tf
@@ -2,9 +2,9 @@ resource "azurerm_kubernetes_cluster_extension" "storage" {
   depends_on = [
     azurerm_kubernetes_cluster.this
   ]
-  name           = "azurecontainerstorage"
-  cluster_id     = azurerm_kubernetes_cluster.this.id
-  extension_type = "microsoft.azurecontainerstorage"
-  release_train  = "prod"
+  name              = "azurecontainerstorage"
+  cluster_id        = azurerm_kubernetes_cluster.this.id
+  extension_type    = "microsoft.azurecontainerstorage"
+  release_train     = "prod"
   release_namespace = "acstor"
 }
