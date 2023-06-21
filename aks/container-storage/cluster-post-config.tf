f20d570a4a50ee16ab58564710adf91e823cd1f2
Brian <brian@briandenicola.com>
2023-06-21 09:35:16 -0500

    Update cluster-post-config.tf

--- aks/container-storage/cluster-post-config.tf
+++ aks/container-storage/cluster-post-config.tf
@@ -6,4 +6,5 @@ resource "azurerm_kubernetes_cluster_extension" "storage" {
   cluster_id     = azurerm_kubernetes_cluster.this.id
   extension_type = "microsoft.azurecontainerstorage"
   release_train  = "prod"
+  release_namespace = "acstor"
 }
