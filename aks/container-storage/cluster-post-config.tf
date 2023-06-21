7a477fedff1edc6f3d7d1b413b7d562615239967
briandenicola <brian@briandenicola.com>
2023-06-21 10:27:10 -0500

    Updated release namespace

--- aks/container-storage/cluster-post-config.tf
+++ aks/container-storage/cluster-post-config.tf
@@ -6,5 +6,5 @@ resource "azurerm_kubernetes_cluster_extension" "storage" {
   cluster_id        = azurerm_kubernetes_cluster.this.id
   extension_type    = "microsoft.azurecontainerstorage"
   release_train     = "prod"
-  release_namespace = "acstor"
+  release_namespace = "acstor-system"
 }
