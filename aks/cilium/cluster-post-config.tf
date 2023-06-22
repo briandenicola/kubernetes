ed72f0156b6955fc5ad9680a0b22a12e4a79b9d9
briandenicola <brian@briandenicola.com>
2023-06-22 12:39:14 -0500

    Version bump

--- aks/cilium/cluster-post-config.tf
+++ aks/cilium/cluster-post-config.tf
@@ -3,7 +3,7 @@ resource "azapi_update_resource" "this" {
     azurerm_kubernetes_cluster.this
   ]
 
-  type        = "Microsoft.ContainerService/managedClusters@2023-01-02-preview"
+  type        = "Microsoft.ContainerService/managedClusters@2023-05-02-preview"
   resource_id = azurerm_kubernetes_cluster.this.id
 
   body = jsonencode({
