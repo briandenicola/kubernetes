da1b58e797a5bb73a5c0754d6b1936d8fbfdaeb5
briandenicola <brian@briandenicola.com>
2023-08-03 15:15:16 -0500

    Removed admin_group_object_ids

--- aks/windows2022/variables.tf
+++ aks/windows2022/variables.tf
@@ -1,7 +1,3 @@
-variable "azure_rbac_group_object_id" {
-  description = "GUID of the AKS admin Group"
-  default     = "15390134-7115-49f3-8375-da9f6f608dce"
-}
 
 variable "region" {
   description = "Azure region to deploy to"
