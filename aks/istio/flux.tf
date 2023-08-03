288c10ecca6a628ad17739630c2adfc9fb692349
briandenicola <brian@briandenicola.com>
2023-08-03 14:11:54 -0500

    Various updates

--- aks/istio/flux.tf
+++ aks/istio/flux.tf
@@ -1,62 +1,37 @@
-# resource "azapi_resource" "flux_install" {
-#   depends_on = [
-#     azurerm_kubernetes_cluster.this
-#   ]
-
-#   type      = "Microsoft.KubernetesConfiguration/extensions@2021-09-01"
-#   name      = "flux"
-#   parent_id = azurerm_kubernetes_cluster.this.id
-
-#   body = jsonencode({
-#     properties = {
-#       extensionType           = "microsoft.flux"
-#       autoUpgradeMinorVersion = true
-#     }
-#   })
-# }
-
 resource "azurerm_kubernetes_cluster_extension" "flux" {
   depends_on = [
-    azapi_update_resource.istio_ingressgateway
+    azurerm_kubernetes_cluster.this
   ]
   name           = "flux"
   cluster_id     = azurerm_kubernetes_cluster.this.id
   extension_type = "microsoft.flux"
 }
 
-resource "azapi_resource" "flux_config" {
+resource "azurerm_kubernetes_flux_configuration" "flux_config" {
   depends_on = [
     azurerm_kubernetes_cluster_extension.flux
   ]
 
-  type      = "Microsoft.KubernetesConfiguration/fluxConfigurations@2022-03-01"
-  name      = "cluster-config"
-  parent_id = azurerm_kubernetes_cluster.this.id
+  name       = "aks-flux-extension"
+  cluster_id = azurerm_kubernetes_cluster.this.id
+  namespace  = "flux-system"
+  scope      = "cluster"
 
-  body = jsonencode({
-    properties : {
-      scope      = "cluster"
-      namespace  = "flux-system"
-      sourceKind = "GitRepository"
-      suspend    = false
-      gitRepository = {
-        url                   = local.flux_repository
-        timeoutInSeconds      = 600
-        syncIntervalInSeconds = 300
-        repositoryRef = {
-          branch = "main"
-        }
-      }
-      kustomizations : {
-        apps = {
-          path                   = local.cluster_path
-          dependsOn              = []
-          timeoutInSeconds       = 600
-          syncIntervalInSeconds  = 120
-          retryIntervalInSeconds = 300
-          prune                  = true
-        }
-      }
-    }
-  })
+  git_repository {
+    url                      = local.flux_repository
+    reference_type           = "branch"
+    reference_value          = "main"
+    timeout_in_seconds       = 600
+    sync_interval_in_seconds = 30
+  }
+
+  kustomizations {
+    name                       = "apps"
+    path                       = local.cluster_path
+    timeout_in_seconds         = 600
+    sync_interval_in_seconds   = 120
+    retry_interval_in_seconds  = 300
+    garbage_collection_enabled = true
+    depends_on                 = []
+  }
 }
