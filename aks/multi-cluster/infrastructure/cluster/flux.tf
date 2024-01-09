4cbafdffcfa5ff7e595e3ae5e34a8ff267e69f27
briandenicola <brian@briandenicola.com>
2024-01-09 10:53:35 -0600

    Removed unused extensions

--- aks/multi-cluster/infrastructure/cluster/flux.tf
+++
@@ -1,8 +0,0 @@
-resource "azurerm_kubernetes_cluster_extension" "flux" {
-  depends_on = [
-    azurerm_kubernetes_cluster.this
-  ]
-  name           = "flux"
-  cluster_id     = azurerm_kubernetes_cluster.this.id
-  extension_type = "microsoft.flux"
-}
