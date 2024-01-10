67debb56e20142c375e78dc74edb6f2947f04848
briandenicola <brian@briandenicola.com>
2024-01-10 15:02:30 -0600

    AKS Module Definition

---
+++ aks/module/logging.tf
@@ -0,0 +1,28 @@
+resource "azurerm_log_analytics_workspace" "this" {
+  name                = "${var.resource_name}-logs"
+  location            = azurerm_resource_group.this.location
+  resource_group_name = azurerm_resource_group.this.name
+  sku                 = "PerGB2018"
+  daily_quota_gb      = 0.5
+}
+
+resource "azurerm_log_analytics_solution" "this" {
+  solution_name         = "ContainerInsights"
+  location              = azurerm_resource_group.this.location
+  resource_group_name   = azurerm_resource_group.this.name
+  workspace_resource_id = azurerm_log_analytics_workspace.this.id
+  workspace_name        = azurerm_log_analytics_workspace.this.name
+
+  plan {
+    publisher = "Microsoft"
+    product   = "OMSGallery/ContainerInsights"
+  }
+}
+
+resource "azurerm_application_insights" "this" {
+  name                = "${var.resource_name}-appinsights"
+  location            = azurerm_resource_group.this.location
+  resource_group_name = azurerm_resource_group.this.name
+  workspace_id        = azurerm_log_analytics_workspace.this.id
+  application_type    = "web"
+}
---
+++ aks/module/variables.tf
@@ -0,0 +1,43 @@
+variable "region" {
+  description = "Azure region to deploy to"
+  default     = "southcentralus"
+}
+
+variable "authorized_ip_ranges" {
+  description = "Authorized IP ranges for accessing the cluster"
+}
+
+variable "resource_name" {
+  description = "The root value to use for naming resources"
+}
+
+variable "public_key_openssh" {
+  description = "The public key to use for SSH access to the cluster"
+}
+
+variable "vm_sku" {
+  description = "The value for the VM SKU"
+  default     = "Standard_D4ads_v5"
+} 
+
+variable "sdlc_environment" {
+  description = "The value for the sdlc environment"
+}
+
+variable kubernetes_version {
+  description = "The version of Kubernetes to deploy"
+}
+
+variable automatic_channel_upgrade {
+  default     =  "patch"
+  description = "The value for the automatic channel upgrade"
+}
+
+variable node_os_channel_upgrade {
+  default     =  "NodeImage"
+  description = "The value for the node OS channel upgrade"
+}
+
+variable "tags" {
+  description = "Tags to apply for this resource"
+}
