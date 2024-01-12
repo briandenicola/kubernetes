51a73de942cdb84807b93bcb79679b94c9aaef22
briandenicola <brian@briandenicola.com>
2024-01-12 11:42:32 -0600

    Added demo apps to deployment

--- aks/kata/network.tf
+++ aks/kata/network.tf
@@ -50,6 +50,18 @@ resource "azurerm_network_security_group" "this" {
   name                = "${local.resource_name}-default-nsg"
   location            = azurerm_resource_group.this.location
   resource_group_name = azurerm_resource_group.this.name
+
+  security_rule {
+    name                       = "http"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "80"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
 }
 
 resource "azurerm_subnet_network_security_group_association" "nodes" {
