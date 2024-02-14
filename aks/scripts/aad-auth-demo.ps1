6deff26c312515d06cfe34ae6408843db9fb2a2e
briandenicola <brian@briandenicola.com>
2024-02-14 14:04:08 -0600

    Updated demo

--- aks/scripts/aad-auth-demo.ps1
+++ aks/scripts/aad-auth-demo.ps1
@@ -6,11 +6,15 @@ param (
     [string] $TenantID,
 
     [Parameter(Mandatory=$true)]
-    [string] $ClientID
+    [string] $ClientID,
+
+    [Parameter(Mandatory=$true)]
+    [string] $ClientSecret
 
 )
 
-$creds = Get-Credential -UserName $ClientID -Message "Please enter the Client Secret for $ClientID"
+#$creds = Get-Credential -UserName $ClientID -Message "Please enter the Client Secret for $ClientID"
+$creds = New-PSCredentials -UserName $ClientID -Password $ClientSecret
 $uri = "https://login.microsoftonline.com/$TenantID/oauth2/token"
 
 if( -not ($AksApiUri.IsAbsoluteUri) ) {
@@ -30,7 +34,21 @@ $auth = Invoke-RestMethod -Uri $uri  -Method Post -Body @{
 
 $h = @{}; $h.Add('Authorization', ('{0} {1}' -f $auth.token_type, $auth.access_token))
 
+Write-Host ("[{0}] - Access token: {1}..." -f (Get-Date), $auth.access_token)
 Write-Host ("[{0}] - Getting all namespaces againist {1} . . ." -f (Get-Date), $AksApiUri.DnsSafeHost)
+Invoke-RestMethod -Method get -SkipCertificateCheck -Headers $h -ContentType "application/json" -Uri $namespaceUri.ToString() |
+     Select-Object @{Name = 'Name'; Expression = {$_.items.metadata.name}} | 
+     Select-Object -ExpandProperty Name
+
+
+if( -not ($AksApiUri.IsAbsoluteUri) ) {
+    $namespaceUri = New-Object System.UriBuilder -ArgumentList ([uri]::UriSchemeHttps), $AksApiUri, 443, "api/v1/namespaces/kube-system/pods"
+} else {
+    $namespaceUri = New-Object System.UriBuilder($AksApiUri)
+    $namespaceUri.Path = "api/v1/namespaces/kube-system/pods"
+}
+
+Write-Host ("[{0}] - Getting all pods in kube-system on {1} . . ." -f (Get-Date), $AksApiUri.DnsSafeHost)
 Invoke-RestMethod -Method get -SkipCertificateCheck -Headers $h -ContentType "application/json" -Uri $namespaceUri.ToString() |
      Select-Object @{Name = 'Name'; Expression = {$_.items.metadata.name}} | 
      Select-Object -ExpandProperty Name
