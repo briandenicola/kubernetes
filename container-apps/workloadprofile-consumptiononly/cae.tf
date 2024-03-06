17e520f29ea0ea0a4682b017e61257292398e341
briandenicola <brian@briandenicola.com>
2024-03-06 16:02:16 -0600

    Moved to Azure Monitor

--- container-apps/workloadprofile-consumptiononly/cae.tf
+++ container-apps/workloadprofile-consumptiononly/cae.tf
@@ -3,7 +3,7 @@ resource "azapi_resource" "azurerm_container_app_environment" {
     azapi_update_resource.nodes_delegation
   ]
 
-  type      = "Microsoft.App/managedEnvironments@2023-04-01-preview"
+  type      = "Microsoft.App/managedEnvironments@2023-05-01"
   name      = local.aca_name
   location  = azurerm_resource_group.this.location
   parent_id = azurerm_resource_group.this.id
@@ -11,18 +11,22 @@ resource "azapi_resource" "azurerm_container_app_environment" {
   body = jsonencode({
     properties = {
       appLogsConfiguration = {
-        destination = "log-analytics"
-        logAnalyticsConfiguration = {
-          customerId = azurerm_log_analytics_workspace.this.workspace_id
-          sharedKey  = azurerm_log_analytics_workspace.this.primary_shared_key
-        }
+        destination = "azure-monitor"
       }
+      
+      daprAIConnectionString      = azurerm_application_insights.this.connection_string
 
       infrastructureResourceGroup = "${local.resource_name}_aca_nodes_rg"
       zoneRedundant               = true
       vnetConfiguration = {
-        infrastructureSubnetId = azurerm_subnet.nodes.id
-        internal               = true
+        infrastructureSubnetId    = azurerm_subnet.nodes.id
+        internal                  = true
+      }
+
+      peerAuthentication = {
+        mtls = {
+          enabled = true
+        }
       }
 
       workloadProfiles = [
