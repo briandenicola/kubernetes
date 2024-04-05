cefacbaf66b3757df7bad639a5ef5e771c42b3db
briandenicola <brian@briandenicola.com>
2024-04-05 08:25:36 -0500

    ⚾ Fri Apr  5 08:25:36 CDT 2024

--- aks/kata/main.tf
+++ aks/kata/main.tf
@@ -1,10 +1,3 @@
-data "azurerm_client_config" "current" {}
-data "azurerm_subscription" "current" {}
-
-data "http" "myip" {
-  url = "http://checkip.amazonaws.com/"
-}
-
 resource "random_id" "this" {
   byte_length = 2
 }
@@ -14,45 +7,8 @@ resource "random_pet" "this" {
   separator = ""
 }
 
-resource "random_password" "password" {
-  length  = 25
-  special = true
-}
-
-resource "random_integer" "vnet_cidr" {
-  min = 10
-  max = 250
-}
-
-resource "random_integer" "services_cidr" {
-  min = 64
-  max = 99
-}
-
-resource "random_integer" "pod_cidr" {
-  min = 100
-  max = 127
-}
-
 locals {
   location             = var.region
   resource_name        = "${random_pet.this.id}-${random_id.this.dec}"
-  aks_name             = "${local.resource_name}-aks"
-  acr_name             = "${replace(local.resource_name, "-", "")}acr"
-  vnet_cidr            = cidrsubnet("10.0.0.0/8", 8, random_integer.vnet_cidr.result)
-  pe_subnet_cidir      = cidrsubnet(local.vnet_cidr, 8, 1)
-  api_subnet_cidir     = cidrsubnet(local.vnet_cidr, 8, 2)
-  nodes_subnet_cidir   = cidrsubnet(local.vnet_cidr, 8, 3)
-  compute_subnet_cidir = cidrsubnet(local.vnet_cidr, 8, 10)
-}
-
-resource "azurerm_resource_group" "this" {
-  name     = "${local.resource_name}_rg"
-  location = local.location
-
-  tags = {
-    Application = "tbd"
-    Components  = "aks;"
-    DeployedOn  = timestamp()
-  }
+  authorized_ip_ranges = "${chomp(data.http.myip.response_body)}/32"
 }
--- aks/kata/variables.tf
+++ aks/kata/variables.tf
@@ -1,16 +1,3 @@
-variable "namespace" {
-  description = "The namespace for the workload identity"
-  type        = string
-  default     = "default"
-}
-
-
-
-variable "service_mesh_type" {
-  description = "Type of Service Mesh for cluster"
-  default     = "istio"
-}
-
 variable "region" {
   description = "Azure region to deploy to"
   default     = "southcentralus"
@@ -24,4 +11,16 @@ variable "vm_size" {
 variable "node_count" {
   description = "The default number of nodes to scale the cluster to"
   default     = 1
+}
+
+variable "nodepool_name" {
+  description = "The name of the node pool"
+  type        = string
+  default     = "system"
+}
+
+variable "tags" {
+  description = "The name of the node pool"
+  type        = string
+  default     = "Azure ML Extension with AKS Cluster Demo"
 }
