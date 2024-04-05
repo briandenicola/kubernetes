eb40bc175e66f76613f8283077a0382b74ca6973
briandenicola <brian@briandenicola.com>
2024-04-05 12:44:28 -0500

    🥊 Fri Apr  5 12:44:28 CDT 2024

--- aks/azureml-extension/workspace-compute.tf
+++ aks/azureml-extension/workspace-compute.tf
@@ -1,12 +1,16 @@
 resource "azurerm_machine_learning_inference_cluster" "this" {
-  name                  = "k8s-compute"
-  location              = local.location
-  cluster_purpose       = "DevTest"
-  kubernetes_cluster_id = data.azurerm_kubernetes_cluster.this.id
+  depends_on                    = [
+    azurerm_kubernetes_cluster_extension.azureml, 
+    azurerm_machine_learning_workspace.this
+  ]
+
+  name                          = "k8s-compute"
+  location                      = local.location
+  cluster_purpose               = "DevTest"
+  kubernetes_cluster_id         = data.azurerm_kubernetes_cluster.this.id
   machine_learning_workspace_id = azurerm_machine_learning_workspace.this.id
 
   identity {
     type = "SystemAssigned"
   }
-
 }
