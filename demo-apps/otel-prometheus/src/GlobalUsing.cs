70b112bec950778ec8f0b82e4009a6aac3a3e8d5
briandenicola <brian@briandenicola.com>
2024-04-23 15:56:24 -0500

    ⚾ Tue Apr 23 15:56:24 CDT 2024

--- demo-apps/otel-prometheus/src/GlobalUsing.cs
+++ demo-apps/otel-prometheus/src/GlobalUsing.cs
@@ -1,13 +1,20 @@
+global using System.Diagnostics;
+global using System.Diagnostics.Metrics;
+
+global using Microsoft.AspNetCore.Mvc;
 global using Microsoft.Extensions.DependencyInjection;
 global using Azure.Monitor.OpenTelemetry.AspNetCore;
+
+global using OpenTelemetry;
 global using OpenTelemetry.Metrics;
 global using OpenTelemetry.Trace;
 global using OpenTelemetry.Logs;
 global using OpenTelemetry.Resources;
-global using System.Diagnostics.Metrics;
-global using System.Diagnostics;
-global using Microsoft.AspNetCore.Mvc;
 
-global using OpenTelemetry.Exporter.Prometheus;
-global using OpenTelemetry;
+
+
+
 global using OpenTelemetry.Exporter;
+global using OpenTelemetry.Exporter.Prometheus;
+
+
