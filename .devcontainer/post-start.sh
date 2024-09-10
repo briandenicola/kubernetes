dc54b460c6ca9ca580d426ea6a4a5cfd5b614d96
briandenicola <brian@briandenicola.com>
2024-09-10 09:19:51 -0500

    🎱 Tue Sep 10 09:19:51 CDT 2024

--- .devcontainer/post-start.sh
+++ .devcontainer/post-start.sh
@@ -6,7 +6,4 @@ echo "$(date)    post-start start" >> ~/status
 sudo az aks install-cli
 sudo az extension add --name application-insights -y
 sudo az extension add --name aks-preview -y
-
-git clone https://github.com/briandenicola/eshop.git /workspaces/eshop
-
 echo "$(date)    post-start complete" >> ~/status
