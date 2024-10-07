e89ebfa801d4f1ca55ef9ece435f60d360b22f71
briandenicola <brian@briandenicola.com>
2024-10-07 11:10:39 -0500

    🥊 Mon Oct  7 11:10:39 CDT 2024

--- aks/container-storage/cluster.tf
+++ aks/container-storage/cluster.tf
@@ -5,7 +5,7 @@ module "cluster" {
   resource_name        = local.resource_name
   public_key_openssh   = tls_private_key.rsa.public_key_openssh
   tags                 = local.tags
-  kubernetes_version   = "1.27.7"
+  kubernetes_version   = "1.30"
   sdlc_environment     = "dev"
   vm_sku               = var.vm_size
   vm_os                = "Ubuntu"
