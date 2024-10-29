1a1914e645e48fc69a4539d9c4bf5e3de2554518
briandenicola <brian@briandenicola.com>
2024-10-29 08:49:51 -0500

    🏀 Tue Oct 29 08:49:50 CDT 2024

--- aks/managed-prometheus/cluster.tf
+++ aks/managed-prometheus/cluster.tf
@@ -1,17 +1,13 @@
-locals {
-  authorized_ip_ranges  = ["${jsondecode(data.http.myip.response_body).ip}/32"]
-  kubernetes_version    = data.azurerm_kubernetes_service_versions.current.versions[length(data.azurerm_kubernetes_service_versions.current.versions) - 1]
-}
-
 module "aks_cluster" {
   source               = "../modules/aks.v3"
-  region                = azurerm_resource_group.this.location
-  authorized_ip_ranges  = local.authorized_ip_ranges
-  resource_name         = local.resource_name
-  public_key_openssh    = tls_private_key.rsa.public_key_openssh
-  sdlc_environment      = "Production"
-  kubernetes_version    = local.kubernetes_version
-  tags                  = local.tags
-  enable_mesh           = false
-  vm_sku                = "Standard_B4ls_v2"
+  region               = azurerm_resource_group.this.location
+  authorized_ip_ranges = local.authorized_ip_ranges
+  resource_name        = local.resource_name
+  public_key_openssh   = tls_private_key.rsa.public_key_openssh
+  sdlc_environment     = "Production"
+  kubernetes_version   = local.kubernetes_version
+  tags                 = local.tags
+  enable_mesh          = false
+  vm_sku               = var.vm_size
+  node_count           = var.node_count
 }
--- aks/managed-prometheus/main.tf
+++ aks/managed-prometheus/main.tf
@@ -9,11 +9,13 @@ resource "random_pet" "this" {
 
 locals {
   location                    = var.region
+  tags                        = var.tags  
   resource_name               = "${random_pet.this.id}-${random_id.this.dec}"
   app_identity_name           = "${local.resource_name}-identity"
   azuremonitor_workspace_name = "${local.resource_name}-prometheus"
   app_insights_name           = "${local.resource_name}-appinsights"
-  tags                        = "Azure Managed Prometheus Demo"
+  kubernetes_version          = data.azurerm_kubernetes_service_versions.current.versions[length(data.azurerm_kubernetes_service_versions.current.versions) - 1]
+  authorized_ip_ranges        = ["${jsondecode(data.http.myip.response_body).ip}/32"]
 }
 
 resource "azurerm_resource_group" "this" {
