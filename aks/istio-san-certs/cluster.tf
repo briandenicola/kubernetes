20e47dfa24371f6001f7da31b2ae7585567008f7
briandenicola <brian@briandenicola.com>
2024-11-14 14:21:33 -0600

    🏓 Thu Nov 14 14:21:33 CST 2024

--- aks/istio-san-certs/cluster.tf
+++ aks/istio-san-certs/cluster.tf
@@ -1,14 +1,15 @@
 module "cluster" {
-  source               = "../modules/aks.v4"
-  region               = var.region
-  zones                = var.zones
-  authorized_ip_ranges = local.authorized_ip_ranges
-  resource_name        = local.resource_name
-  public_key_openssh   = tls_private_key.rsa.public_key_openssh
-  tags                 = var.tags
-  kubernetes_version   = "1.29"
-  sdlc_environment     = "dev"
-  vm_sku               = var.vm_size
-  node_count           = var.node_count
-  enable_mesh          = false 
+  source                             = "../modules/aks.v4"
+  region                             = var.region
+  zones                              = var.zones
+  authorized_ip_ranges               = local.authorized_ip_ranges
+  resource_name                      = local.resource_name
+  public_key_openssh                 = tls_private_key.rsa.public_key_openssh
+  tags                               = var.tags
+  kubernetes_version                 = "1.29"
+  sdlc_environment                   = "dev"
+  vm_sku                             = var.vm_size
+  node_count                         = var.node_count
+  enable_mesh                        = false
+  azurerm_log_analytics_workspace_id = module.azure_monitor.LOG_ANALYTICS_WORKSPACE_ID
 }
