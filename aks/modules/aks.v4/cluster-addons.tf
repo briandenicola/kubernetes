b1ce64ee5f4fff234eabfeedd44b36f7440d7cfe
briandenicola <brian@briandenicola.com>
2024-12-04 11:32:57 -0600

    🥊 Wed Dec  4 11:32:57 CST 2024

--- aks/modules/aks.v4/cluster-addons.tf
+++ aks/modules/aks.v4/cluster-addons.tf
@@ -3,11 +3,17 @@ resource "azapi_update_resource" "cluster_updates" {
     azurerm_kubernetes_cluster.this
   ]
 
-  type        = "Microsoft.ContainerService/managedClusters@2024-03-02-preview"
+  type        = "Microsoft.ContainerService/managedClusters@2024-07-02-preview"
   resource_id = azurerm_kubernetes_cluster.this.id
 
   body = jsonencode({
     properties = {
+      agentPoolProfiles = [{
+        name = "system"
+        upgradeSettings = {
+          undrainableNodeBehavior = "Cordon"
+        }
+      }]
       networkProfile = {
         advancedNetworking = {
           observability = {
