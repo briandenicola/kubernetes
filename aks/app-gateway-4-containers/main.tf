7ffb6aed47e5114d42424a8a9ae8fc26303c57de
briandenicola <brian@briandenicola.com>
2025-03-20 12:16:00 -0500

    🏀 Thu Mar 20 12:16:00 CDT 2025

--- aks/app-gateway-4-containers/main.tf
+++ aks/app-gateway-4-containers/main.tf
@@ -12,7 +12,7 @@ locals {
   resource_name        = "${random_pet.this.id}-${random_id.this.dec}"
   authorized_ip_ranges = ["${chomp(data.http.myip.response_body)}/32"]
   alb_name             = "${local.resource_name}-alb"
-  alb_identity_name    = "${local.resource_name}-identity"
+  alb_identity_name    = "${local.resource_name}-alb-identity"
   environment_type     = "dev"
   k8s_version          = "1.31"
   app_path             = "./aks/app-gateway-4-containers/cluster-config"
