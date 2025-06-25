17d6219bb107f03a04b88c239a6067fa391e4c70
briandenicola <brian@briandenicola.com>
2025-06-25 10:28:23 -0400

    🥊 Wed Jun 25 10:28:23 EDT 2025

--- aks/container-storage/roles.tf
+++ aks/container-storage/roles.tf
@@ -8,3 +8,14 @@ resource "azurerm_role_assignment" "container_storage_role" {
   principal_id                     = data.azurerm_kubernetes_cluster.this.kubelet_identity[0].object_id
   skip_service_principal_aad_check = true
 }
+
+resource "azurerm_role_assignment" "cluster_admin_role" {
+  count = var.deploy_jumpbox ? 1 : 0
+  depends_on = [
+    module.cluster,
+    module.vm
+  ]
+  scope                            = module.cluster.AKS_CLUSTER_ID
+  role_definition_name             = "Azure Kubernetes Service RBAC Cluster Admin"
+  principal_id                     = module.vm[0].VM_PRINCIPAL_ID
+}
